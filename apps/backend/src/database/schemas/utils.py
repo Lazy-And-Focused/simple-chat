dfecb58df460204f4911122de9f786b7ab17bf2f
FOCKUSTY <wwddwwwd@mail.ru>
2025-08-20 15:33:22 +0500

    -database

--- apps/backend/src/database/schemas/utils.py
+++
@@ -1,6 +0,0 @@
-from typing import Any
-
-from sqlalchemy.orm import Session
-
-def create(data: Any, session: Session):
-    session.add(data)
--- apps/backend/src/database/session.py
+++ apps/backend/src/database/session.py
@@ -1,6 +1,7 @@
+from typing import Any
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select 
 
-from .schemas.utils import create
 from .schemas import UserBase
 from .engine import engine, createTables
 
@@ -9,8 +10,25 @@ createTables()
 Session = sessionmaker(engine)
 session = Session()
 
+def create(data: Any):
+    return session.add(data)
+
+def update(data: Any):
+    return session.merge(data)
+
+def getById(id: int, base: Any):
+    statement = select(base).where(base.id == id) # type: ignore
+    data = session.scalars(statement).one()
+
+    return data
+
+# def delete(id: str):
+
+
 try:
-    create(UserBase(user_name="fockusty"), session)
+    # create(UserBase(user_name="fockusty"))
+
+    print(getById(1, UserBase))
 except:
     session.rollback()
     raise
