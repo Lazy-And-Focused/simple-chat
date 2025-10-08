90e87edd894a52af146816d04fb48070d60c4be4
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-10-08 19:23:22 +0500

    -init chatting

--- apps/test/frontend/src/app/rooms/[id]/page.tsx
+++ apps/test/frontend/src/app/rooms/[id]/page.tsx
@@ -4,12 +4,18 @@ import type { User } from "types/user.type";
 import type { Message } from "types/message.type";
 
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { getToken } from "api/get-token";
 import { getUser } from "api/get-user";
 
 import styles from "./page.module.css";
+
+const resolveClassName = (authorId: number, userId: number) => {
+  return authorId === userId
+    ? "self_message"
+    : "alien_message";
+}
 
 const Page = () => {
   const [ socket, setSocket ] = useState<WebSocket|null>(null);
@@ -17,7 +23,10 @@ const Page = () => {
   const [ loaded, setLoaded ] = useState<boolean>(false);
   const [ connected, setConnect ] = useState<boolean>(false);
 
-  const [ messages, setMessages ] = useState<Message[]>([]);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const [ messages, setMessages ] = useState<Message[]>([
+    {author: "Anon", author_id: 0, text: "Test"}
+  ]);
 
   const { id } = useParams<{ id: string }>();
 
@@ -41,6 +50,12 @@ const Page = () => {
       });
 
       setLoaded(true);
+
+      webSocket.addEventListener("message", (message) => {
+        const data = JSON.parse(message.data) as Message;
+
+        setMessages((previous) => [...previous, data]);
+      })
     })();
 
     return () => {
@@ -72,14 +87,37 @@ const Page = () => {
   return (
     <div className={styles.main}>
       <span>Привет, {user.username}, Вы подключены</span>
-      <div className={styles.chat}>
-        {
-          messages.map(message => (
-            <div className={`${styles.message} ${styles["message_" + `${message.author_id === user.id}`]}`}>
-              <span>{message.author}: {message.text}</span>
-            </div>
-          ))
-        }
+      <div className={styles.chat_window}>
+        <div className={styles.chat}>
+          {
+            messages.map((message, i) => (
+              <div key={i} className={[
+                styles.message,
+                styles[resolveClassName(message.author_id, user.id)]
+              ].join(" ")}>
+                <span>{message.author}: {message.text}</span>
+              </div>
+            ))
+          }
+        </div>
+        <form className={"message " + styles.input} onSubmit={(e) => {
+          e.preventDefault();
+
+          if (!socket || !textRef.current) {
+            return;
+          }
+
+          socket.send(JSON.stringify({
+            text: textRef.current.value,
+            author: user.username,
+            author_id: user.id
+          }));
+
+          textRef.current.value = "";
+        }}>
+          <textarea ref={textRef} placeholder="Введите сообщение" name="message"></textarea>
+          <input type="submit" value="Готово" />
+        </form>
       </div>
     </div>
   )
